08705e607522047d7a72b7881d018e6d682ab448
Bridget Leonard <bridget@Bridgets-MacBook-Air-4.local>
2024-06-14 20:04:36 -0700

    add nvda test

---
+++ nvda.py
@@ -0,0 +1,16 @@
+from edgar import *
+
+set_identity("Bridget Leonard leonardbridget01@gmail.com")
+
+nvda_10k = Company("NVDA").get_filings(form="10-K").latest(1)
+
+# Calling .obj() on filing automatically downloads and parses data files into data object
+tenk = nvda_10k.obj()
+print(tenk)
+
+# Can also extract financial statements from 10-k and 10-q
+financials = tenk.financials
+print(financials.balance_sheet)
+
+# Use to_dataframe method to get as pd df
+balance_sheet_df = financials.balance_sheet.to_dataframe()
